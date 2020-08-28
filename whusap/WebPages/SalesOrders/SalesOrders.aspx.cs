db0447f5c038e5e1c045b2b51245cb6436c1d5e3
Juancho <juanccubillos@gmail.com>
2020-08-28 13:07:57 -0500

    cambios insert a 08 desde sales orders

--- whusap/WebPages/SalesOrders/SalesOrders.aspx.cs
+++ whusap/WebPages/SalesOrders/SalesOrders.aspx.cs
@@ -27,11 +27,12 @@ namespace whusap.WebPages.SalesOrders
         public static IntefazDAL_ttccom110 Ittccom110 = new IntefazDAL_ttccom110();
         public static IntefazDAL_twhinh220 Itwhinh220 = new IntefazDAL_twhinh220();
         public static IntefazDAL_tticol082 Itticol082 = new IntefazDAL_tticol082();
+        public static InterfazDAL_tticol080 Itticol080 = new InterfazDAL_tticol080();
         public static InterfazDAL_twhcol130 Itwhcol130 = new InterfazDAL_twhcol130();
         private static InterfazDAL_ttccol301 _idalttccol301 = new InterfazDAL_ttccol301();
 
+        public static string strError = string.Empty;
         public static string InvalidCustomerCode = string.Empty;
-
         string formName = string.Empty;
         public static string _operator = string.Empty;
         string  _idioma = string.Empty;
@@ -247,30 +248,53 @@ namespace whusap.WebPages.SalesOrders
 
                     if (ciclosADVS > 0)
                     {
-                        int PRIO = Itticol082.PrioridadMaxima();
+                        //int PRIO = Itticol082.PrioridadMaxima();
+                        //int PRIO = Itticol080.PrioridadMaxima();
 
                         for (int i = 1; i <= ciclosADVS; i++)
                         {
-                            Ent_tticol082 Objtticol082 = new Ent_tticol082
+                            //Ent_tticol082 Objtticol082 = new Ent_tticol082
+                            //{
+                            //    PAID = " ",
+                            //    LOGN = _operator,
+                            //    OORG = myObj.OORG,
+                            //    ORNO = myObj.ORNO,
+                            //    OSET = myObj.OSET,
+                            //    PONO = myObj.PONO,
+                            //    SQNB = myObj.SEQN,
+                            //    ADVS = i.ToString(),
+                            //    ITEM = myObj.ITEM,
+                            //    STAT = "1",
+                            //    QTYT = CantidadPorCiclo(QUANTITYCUNI, Convert.ToInt32(MyConvertionFactor.FactorD), ciclosADVS, i).ToString(),
+                            //    CWAR = myObj.SFCO.Trim(),
+                            //    UNIT = myObj.CUNI,
+                            //    PRIO = Convert.ToString(PRIO+1)
+                            //};
+
+                            Ent_tticol080 Objtticol080 = new Ent_tticol080
                             {
-                                PAID = " ",
-                                LOGN = _operator,
-                                OORG = myObj.OORG,
-                                ORNO = myObj.ORNO,
-                                OSET = myObj.OSET,
-                                PONO = myObj.PONO,
-                                SQNB = myObj.SEQN,
-                                ADVS = i.ToString(),
-                                ITEM = myObj.ITEM,
-                                STAT = "1",
-                                QTYT = CantidadPorCiclo(QUANTITYCUNI, Convert.ToInt32(MyConvertionFactor.FactorD), ciclosADVS, i).ToString(),
-                                CWAR = myObj.SFCO.Trim(),
-                                UNIT = myObj.CUNI,
-                                PRIO = Convert.ToString(PRIO+1)
+
+                                oorg = myObj.OORG,
+                                orno = myObj.ORNO,
+                                pono = Convert.ToInt32(myObj.PONO),
+                                item = myObj.ITEM,
+                                cwar = myObj.SFCO.Trim(),
+                                qune = Convert.ToDecimal(CantidadPorCiclo(QUANTITYCUNI, Convert.ToInt32(MyConvertionFactor.FactorD), ciclosADVS, i).ToString()),
+                                logn = _operator,
+                                proc = 1,
+                                pick = 1,
+                                clot = " ",
+                                refcntd = 0,
+                                refcntu = 0
+
                             };
-                            bool InsertSuccess = Itticol082.InsertarregistroItticol082(Objtticol082);
-                            ListInsertResult.Add(InsertSuccess);
-                            PRIO++;
+                            //bool InsertSuccess = Itticol082.InsertarregistroItticol082(Objtticol082);
+                            List<Ent_tticol080> lst80 = new List<Ent_tticol080>();
+                            lst80.Add(Objtticol080);
+                            string aux = string.Empty;
+                            int InsertSuccess = Itticol080.insertarRegistro(ref lst80, ref strError,ref aux);
+                            ListInsertResult.Add((InsertSuccess != -1 ? true : false));
+                            //PRIO++;
                         }
                     }
                 }
