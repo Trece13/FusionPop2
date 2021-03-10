c62e274f4aa899cec1365ff88411ae66c97e29f1
Jcastro90@hotmail.com <jcastro@grupophoenix.com>
2021-03-10 14:31:48 -0500

    Ajuste Codigo de Barras, reimpresion materia prima

--- whusap/WebPages/InvReceipts/whInvReprintReceiptRawMaterial.aspx.designer.cs
+++ whusap/WebPages/InvReceipts/whInvReprintReceiptRawMaterial.aspx.designer.cs
@@ -13,13 +13,13 @@ namespace whusap.WebPages.InvReceipts {
     public partial class whInvReprintReceiptRawMaterial {
         
         /// <summary>
-        /// Contenido_CBPalletNO control.
+        /// CBPalletNO control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlImage Contenido_CBPalletNO;
+        protected global::System.Web.UI.HtmlControls.HtmlImage CBPalletNO;
         
         /// <summary>
         /// CBPurchaseOrder control.
