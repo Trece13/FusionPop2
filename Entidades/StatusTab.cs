fbaa80c7c86999f769386ab9c0865c19db8fa8e3
Juancho <juanccubillos@gmail.com>
2021-10-13 13:33:39 -0500

    CAmbios

---
+++ Entidades/StatusTab.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace whusa
+{
+    public class StatusTab
+    {
+        public string Table { get; set; }
+        public string Code { get; set; }
+        public string Desc { get; set; }
+    }
+}
